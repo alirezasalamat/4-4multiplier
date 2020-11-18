95fd963fc80929181559f9bcffbf32ff7b673f63
Alireza Salamat <52575958+alirezasalamat@users.noreply.github.com>
2020-11-18 20:25:41 +0330

    register 4 bit edited and multiplier added

--- register_4bit.v
+++ register_4bit.v
@@ -1,15 +1,17 @@
-module register_8bit(clk, rst, ld, in, out);
+module register_4bit(clk, rst, ld, in, out_1, out_2);
 
     input clk, rst, ld;
     input [4:0] in;
-    output reg [4:0] out;
+    output reg [1:0] out_1, out_2;
 
     always @(posedge clk or posedge rst) begin
         if(rst) begin
-            out <= 8'b0;
+            out_1 <= 2'b0;
+            out_2 <= 2'b0;
         end
         else if(ld) begin
-            out <= in;
+            out_1 <= in[3:2];
+            out_2 <= in[1:0];
         end
     end
 endmodule
@@ -18,20 +20,20 @@ module test_register_4bit();
 
     reg clk = 1'b0 , rst, ld;
     reg [4:0] in;
-    wire [4:0] out;
+    wire [1:0] out_1, out_2;
 
-    register_8bit reg8(clk, rst, ld, in, out);
+    register_4bit reg4(clk, rst, ld, in, out_1, out_2);
     
     always #10 clk = ~clk;
 
     initial begin
     #10 ld = 1;
-    in = 8'b10101010;
+    in = 4'b1010;
     #20 ld = 0;
     #200
-    in = 8'b00100101;
+    in = 4'b0010;
     #100 ld = 1;
-    in = 8'b00000001;
+    in = 4'b0000;
     #100 ld = 0;
     #100 $stop;
     end
